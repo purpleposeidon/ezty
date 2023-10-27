9402ff79f10fb0c432cc2034d4c220c39aafdf46
purpleposeidon <purpleposeidon@gmail.com>
2023-10-27 03:23:19 -0700

    Finally figured out how to sanely call get_ty().

--- src/any_debug.rs
+++ src/any_debug.rs
@@ -6,6 +6,9 @@ pub trait AnyDebug: mopa::Any + fmt::Debug + Send + Sync {
     fn type_name(&self) -> &'static str {
         type_name::<Self>()
     }
+    /// You're inevitably wanting to call this with something like `Box<dyn AnyDebug>`, but instead
+    /// of returning the ty of the contents, it uselessly returns `Ty::of::<Box<dyn AnyDebug>>()`.
+    /// How do you deal with this? Instead of `this.get_ty()`, call `(*this).get_ty()`.
     fn get_ty(&self) -> Ty;
 }
 mopafy!(AnyDebug);
@@ -35,5 +38,23 @@ mod test {
         let f: &dyn AnyDebug = f;
         assert_eq!(ty, f.get_ty());
     }
-}
 
+    #[test]
+    fn get_ty_is_still_hard() {
+        let ty = Ty::of::<i32>();
+        assert_eq!(format!("{ty:?}"), "i32");
+        let f: Box<i32> = Box::new(0);
+        let f: Box<dyn AnyDebug> = f;
+        assert_eq!(ty, <dyn AnyDebug>::get_ty(&*f));
+        assert_eq!(ty, (*f).get_ty());
+    }
+
+    #[test]
+    fn what_about_std_just_outta_curiosity() {
+        use std::any::{TypeId, Any};
+        let f: Box<i32> = Box::new(0);
+        let f: Box<dyn Any> = f;
+        let ty = TypeId::of::<i32>();
+        assert_eq!(ty, (*f).type_id());
+    }
+}
